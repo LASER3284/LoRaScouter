0d13f677c4c2d6ec0ee0f71c83c8fcee8b01fe1d
Joe Iliff <jiliff726@gmail.com>
2022-10-19 12:55:41 -0500

    Extra changes

--- Receiver/ReceiverReceiver.py
+++ Receiver/ReceiverReceiver.py
@@ -1,4 +1,5 @@
 import argparse
+import hashlib
 import json
 from typing import Dict, List, Union
 import serial
@@ -6,19 +7,70 @@ import serial.tools.list_ports
 import traceback
 import signal   
 import time
+import os
+import csv
 import sys
 
 _SCOUTING_PACKET_EOF = b"\x00\xFF\x32\x84\xFF\x00"
+_COMBINED_SCOUTING_JSON = "./combined_scouts.json"
+_COMBINED_SCOUTING_CSV = "./combined_scouts.csv"
 
 def handleScoutingData(data: Dict[str, Dict[str, Union[List[Dict[str, Union[str, bool, int, float]]], str]]]):
     print(f"Received new scout data...")
+    combined_scouts: Dict[str, Dict[str, Union[List[Dict[str, Union[str, bool, int, float]]], str]]] = {"teams": {}}
+    if os.path.exists(_COMBINED_SCOUTING_JSON):
+        with open(_COMBINED_SCOUTING_JSON, "r+") as f:
+            combined_scouts = json.load(f)
+
+    for team, scouts in data['teams'].items():
+        for scout in scouts:
+            scout_hash = hashlib.md5(json.dumps(scout).encode('ascii')).hexdigest()
+            if team in combined_scouts["teams"]:
+                hashes = [hashlib.md5(json.dumps(s).encode('ascii')).hexdigest() for s in combined_scouts["teams"][team]]
+                # It looks like we already have this match data scouted, skip it
+                if scout_hash in hashes:
+                    continue
+            
+                combined_scouts["teams"][team].append(scout)
+            else:
+                combined_scouts["teams"].update({ team : [scout]})
+    
+    if "template" not in combined_scouts and "template" in data:
+        combined_scouts["template"] = data["template"]
+
+    with open(_COMBINED_SCOUTING_JSON, "w+") as f:
+        json.dump(combined_scouts, f, indent=4)
+
+    with open(_COMBINED_SCOUTING_CSV, "w+", newline='') as f:
+        writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        writer.writerow(['Team Number', *[value for value in combined_scouts["template"].values()]])
+        
+        for team, scouts in combined_scouts['teams'].items():
+            for scout in scouts:
+                # This is mainly just to guarantee that the CSV rows have the same order as the header.
+                # In theory, it always should, but it can't really hurt.
+                ordered_values = [scout[x] for x in combined_scouts["template"].keys()]
+                writer.writerow([team, *ordered_values])
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--port", default="auto")
     parser.add_argument("--baud", default=9600, type=int)
+    parser.add_argument("--timeout", default=35.00, type=float)
+
+    parser.add_argument("--import-file", type=str, default=None)
     args = parser.parse_args()
+
+    # Change what we're doing based on the import flag
+    if args.import_file is not None:
+        if not os.path.exists(args.import_file):
+            raise Exception(f"Unable to find file at {args.import_file}!!")
+        with open(args.import_file, "r+") as file:
+            data = json.load(file)
+            handleScoutingData(data)
+        
+        return
 
     port = args.port
 
@@ -35,7 +87,7 @@ def main():
         serial_device: serial.Serial = None # type: ignore
         while serial_device is None:
             try:
-                serial_device = serial.Serial(port[0], args.baud, timeout=10.50)
+                serial_device = serial.Serial(port[0], args.baud, timeout=args.timeout)
                 serial_device.isOpen()
             except IOError: # If the port is already opened, close it and open it again and then try again
                 if serial_device is not None:
@@ -53,11 +105,15 @@ def main():
                     data = serial_device.read_until(_SCOUTING_PACKET_EOF)
                     if data is None: 
                         continue
+                    if len(data) <= len(_SCOUTING_PACKET_EOF):
+                        continue
+                    
                     data = data.strip(_SCOUTING_PACKET_EOF)
                     hex_data = " ".join([hex(byte) for byte in data])
                     print(f"Received data (length: {len(data)}): {data} :: {hex_data}")
                     
-                    if len(data) <= len(_SCOUTING_PACKET_EOF):
+                    if not data.endswith(_SCOUTING_PACKET_EOF):
+                        print(f"Received malformed data... Missing packet EOF")
                         continue
 
                     json_data = json.loads(data.decode('ascii'))
